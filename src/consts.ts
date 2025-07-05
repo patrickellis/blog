3ffda374f75b0642cbd7bc57dc21c3e9577b1f4a
PatrickEllis <patrick.ellis.w1@gmail.com>
2025-07-05 21:43:53 +0100

    feat: implement tag pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Patrick Ellis's Blog";
+export const SITE_TITLE = 'Patrick Ellis | Blog';
 export const SITE_DESCRIPTION = 'A collection of my thoughts and writings on software, technology, and life.';
